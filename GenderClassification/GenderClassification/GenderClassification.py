f8b2f2f482115eeb8e53e121932077e74d0f0ab2
Michael Tran <revengex@gamerarmy.com>
2019-02-25 18:01:17 -0600

    Loading data with panda

--- GenderClassification/GenderClassification/GenderClassification.py
+++ GenderClassification/GenderClassification/GenderClassification.py
@@ -1,12 +1,19 @@
+#import nltk
+#from nltk.util import ngrams
 
-import nltk
-from nltk.util import ngrams
+#def word_grams(words, min=1, max=4):
+#    s = []
+#    for n in range(min, max):
+#        for ngram in ngrams(words, n):
+#            s.append(' '.join(str(i) for i in ngram))
+#    return s
 
-def word_grams(words, min=1, max=4):
-    s = []
-    for n in range(min, max):
-        for ngram in ngrams(words, n):
-            s.append(' '.join(str(i) for i in ngram))
-    return s
+#print(word_grams('one two three four'.split(' ')))
 
-print(word_grams('one two three four'.split(' ')))
+import pandas as pd
+
+names = ['Text', 'Classification']
+df = pd.read_excel('data/blog-gender-dataset.xlsx', header=None, names=names, usecols="A,B")
+print(df.columns)
+print(df['Classification'][0])
+print(df['Text'][0])
